cd13a497237cb418c2817460f15d2478b5cecaee
Jonas Wedin <jonas@lechuck.se>
2018-09-19 11:28:43 +0200

    Final working version

--- FolkWiki.py
+++ FolkWiki.py
@@ -1,4 +1,4 @@
-import sys, os, re, argparse, requests
+import sys, os, re, argparse, requests, urllib
 
 def main():
     welcome_message = '''
@@ -17,18 +17,96 @@ def main():
     parser.add_argument("-o", "--output", help="file to save csv to", required=True)
     args = parser.parse_args()
     #get all songs
+    print("Checking %s for songs..." % args.url)
     songs_to_download = get_song_list(args.url, args.filter)
     print("Found %d songs at %s" % (len(songs_to_download), args.url))
+    if not os.path.isdir('./%s' % args.download_path):
+        create_dir = input("Could not find directory %s, create it? [Y/n]" % args.download_path)
+        if create_dir == 'y' or create_dir == 'Y':
+            os.makedirs('./%s' % args.download_path)
+        else:
+            print("Aborting bc no download directory")
+            quit()
     print("Starting download to: %s" % args.download_path)
+    download_all_songs(songs_to_download, args.download_path)
+    if os.path.isfile('./%s' % args.output):
+        overwrite_file = input("File %s already exists, overwrite it?[Y/n]" % args.output)
+        if overwrite_file != 'Y' and overwrite_file != 'y':
+            print("Aborting bc file already exists.")
+            quit()
+    print("Creating csv file %s" % args.output)
+    create_csv_file(args.download_path, args.output)
+    print("Done")
+
+def create_csv_file(download_dir, output_file):
+    csv = []
+    #first part of csv file is description
+    valid_info = ['X','T','C','M','L','R','Q','K','notes']
+    csv.append(valid_info)
+    for filename in os.listdir(download_dir):
+        file_path = "%s/%s" % (download_dir, filename)
+        with open(file_path, 'r', encoding='iso-8859-1') as f:
+            lines = f.readlines()
+            #skip empty files
+            if len(lines) == 0:
+                continue
+            for filtered in _filter_song(lines, valid_info):
+                csv.append(filtered)
+    with open(output_file, 'w') as f:
+        for c in csv:
+            f.write(','.join(c))
+            f.write('\n')
+    print("Written to %s" % output_file)
+
+def _filter_song(song_lines, valid_info):
+    #Function to reduce linesize
+    def r(key, hay):
+        return hay.get(key, "")
+    v = {}
+    songs_in_key = {}
+    current_parsing_song = False
+    current_parsing_song_key = None
+    for line in song_lines:
+        if len(line.strip()) == 0:
+            continue
+        s = line.split(":")
+        if s[0] == 'K' and len(s) == 2:
+            current_parsing_song_key = s[1].strip()
+            current_parsing_song = True
+            songs_in_key[current_parsing_song_key] = []
+            #Skip to the next line with actual song
+            continue
+        if current_parsing_song:
+            songs_in_key[current_parsing_song_key].append(line.strip())
+            #more song lines
+            continue
+        if s[0] in valid_info:
+            v[s[0]] = s[1].strip()
+    #One file can contain several keys
+    for key,song in songs_in_key.items():
+        ret = [ r(x,v) for x in valid_info ]
+        ret.append(key)
+        ret.append(''.join(song))
+        yield ret
+
+def download_all_songs(song_list, download_dir):
+    count = 1
+    for song in song_list:
+        print('\rDownloading song %d/%d...' % (count, len(song_list)), end="")
+        song_without_url = song.split("/")[-1]
+        file_path = '%s/%s' % (download_dir, song_without_url)
+        urllib.request.urlretrieve(song, file_path)
+        count += 1
+    print('\rDownload complete                    ')
 
 def get_song_list(url, song_filter):
     #Fetch source
-    # response = requests.get(url)
-    # data = response.text
-    # data = data.split('\n')
+    response = requests.get(url)
+    data = response.text
+    data = data.split('\n')
     #temp
-    with open('cache', 'r', encoding='iso-8859-1') as f:
-        data = f.readlines()
+    # with open('cache', 'r', encoding='iso-8859-1') as f:
+        # data = f.readlines()
     #filter out all lines with links in them
     links = [ x for x in data if 'href' in x and song_filter in x ]
     #Replace the . in filter for regex safe \.
