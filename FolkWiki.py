2e84a6ecde01c87ca77794e9087b6e472b1938df
Jonas Wedin <jonas@lechuck.se>
2018-09-26 19:58:09 +0200

    it works, refactor time

--- FolkWiki.py
+++ FolkWiki.py
@@ -1,4 +1,5 @@
 import sys, os, re, argparse, requests, urllib, re
+from pprint import pprint
 
 def main():
     welcome_message = '''
@@ -17,6 +18,7 @@ def main():
     parser.add_argument("-o", "--output", help="file to save", required=True)
     parser.add_argument("--folk_rnn_style", help="Save as folk-rnn style instead of csv", action='store_true')
     parser.add_argument("--skip_download", help="Skips downloading of songs and just creates file from download_path", action='store_true')
+    parser.add_argument("--skip_chords", help="Don't include chords in final product", action='store_true')
     args = parser.parse_args()
     #get all songs
     if args.skip_download:
@@ -41,15 +43,16 @@ def main():
                 quit()
     if args.folk_rnn_style:
         print("Creating folk-rnn style %s" % args.output)
-        create_folk_rnn_file(args.download_path, args.output)
+        create_folk_rnn_file(args.download_path, args.output, args.skip_chords)
     else:
         print("Creating csv file %s" % args.output)
         create_csv_file(args.download_path, args.output)
     print("Done")
 
-def create_folk_rnn_file(download_dir, output_file):
+def create_folk_rnn_file(download_dir, output_file, skip_chords):
     folk_rnn_file = []
-    valid_info = ['T', 'M', 'L']
+    valid_info = ['M', 'L']
+    all_tokens_set = set()
     for filename in os.listdir(download_dir):
         file_path = "%s/%s" % (download_dir, filename)
         with open(file_path, 'r', encoding='iso-8859-1') as f:
@@ -57,51 +60,103 @@ def create_folk_rnn_file(download_dir, output_file):
             #Skip empty lines
             if len(lines) == 0:
                 continue
-            for filtered in _filter_song(lines, valid_info):
-                # print("T:%s" % filtered[0])
-                # print("L:%s" % filtered[2])
-                # print("M:%s" % filtered[1])
-                # print("K:%s" % filtered[3])
-                # print(filtered[4].replace(" ", ""))
-                regex_chord = r"\"[CDEFGAB](b|bb)?(maj7|maj|min7|min|sus|m)?(1|2|3|4|5|6|7|8|9)?(#)?\""
-                m = re.search(regex_chord, filtered[4].replace(" ", ""))
-                if m is not None:
-                    print(m)
+            lines_with_values, tokens = _filter_song_folk_rnn(lines, valid_info, skip_chords)
+            if len(lines_with_values) == 0 or len(tokens) == 0:
+                continue
+            for tok in tokens:
+                all_tokens_set.add(tok)
+            for l in lines_with_values:
+                folk_rnn_file.append(l)
+            folk_rnn_file.append(' '.join(tokens))
+            folk_rnn_file.append('')
+    with open(output_file, 'w') as output:
+        for line in folk_rnn_file:
+            output.write("%s\n" % line)
                 
-
-
-
-def _filter_song_folk_rnn(song_lines, valid_info):
-    #Function to reduce linesize
-    def r(key, hay):
-        return hay.get(key, "")
-    v = {}
-    songs_in_key = {}
-    current_parsing_song = False
-    current_parsing_song_key = None
+#Everything after the first K will be treated as the song and
+#concatenaded into one line
+def _filter_song_folk_rnn(song_lines, valid_info, skip_chords):
+    lines_to_return = []
+    tokens = []
+    currently_parsing_song = False
     for line in song_lines:
-        if len(line.strip()) == 0:
+        if len(line.strip()) == 0 or line[0] == '%':
             continue
-        s = line.split(":")
-        if s[0] == 'K' and len(s) == 2:
-            current_parsing_song_key = s[1].strip()
-            current_parsing_song = True
-            songs_in_key[current_parsing_song_key] = []
-            #Skip to the next line with actual song
-            continue
-        if current_parsing_song:
-            songs_in_key[current_parsing_song_key].append(line.strip())
-            #more song lines
-            continue
-        if s[0] in valid_info:
-            v[s[0]] = s[1].strip()
-    #One file can contain several keys
-    for key,song in songs_in_key.items():
-        ret = [ r(x,v) for x in valid_info ]
-        ret.append(key)
-        ret.append(''.join(song))
-        yield ret
+        if not currently_parsing_song:
+            s = line.split(":")
+            if s[0] == 'K' and len(s) == 2:
+                key_with_brackets = "[%s]" % line.split('%')[0].replace(" ", "").strip()
+                lines_to_return.append(key_with_brackets)
+                currently_parsing_song = True
+                #Skip to the next line with actual song
+                continue
+            elif s[0] in valid_info:
+                validated_line = line.split('%')[0].replace(" ", "")
+                # print(validated_line)
+                lines_to_return.append(validated_line.strip())
+        else:
+            #Currently parsing song
+            tokens = _filter_song_line(line.strip(), skip_chords)
+    return lines_to_return, tokens
 
+def _filter_song_line(song_line, skip_chords):
+    regex_chord = re.compile(r"\"[CDEFGAB](b|bb)?(maj7|maj|min7|min|sus|m)?(1|2|3|4|5|6|7|8|9)?(#)?\"", re.IGNORECASE)
+    regex_tempo = re.compile(r"\[?L\:\s?\d+\/\d+\s?\]?", re.IGNORECASE)
+    regex_meter = re.compile(r"\[?M\:\s?\d+\/\d+\s?\]?", re.IGNORECASE)
+    #For keys inside a song (key change)
+    regex_key = re.compile(r"\[K:\s?[ABCDEFG][#b]?\s?(major|maj|m|minor|min|mixolydian|mix|dorian|dor|phrygian|phr|lydian|lyd|locrian|loc)?\]", re.IGNORECASE)
+    regex_accent = re.compile(r"!.*!")
+    regex_print_specification = re.compile(r"\"@.+\"")
+    regex_repeat_bar_end = re.compile(r":\|")
+    regex_repeat_bar_start = re.compile(r"\|:")
+    regex_verse_marker = re.compile(r"\[V:.+\]")
+    regex_list_tokens = [regex_tempo, regex_meter, regex_key, regex_repeat_bar_end, regex_repeat_bar_start]
+    regex_list_ignore = [regex_verse_marker, regex_accent, regex_print_specification]
+    if skip_chords:
+        regex_list_ignore.append(regex_chord)
+    else:
+        regex_list_tokens.append(regex_chord)
+    ignore_list = '\n \\'
+    #These are all the tokens from the original dataset folk-rnn
+    valid_single_tokens =  ["'",'(', ',', '/', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', '<', '=', '>', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'K', 'M', '[', ']', '^', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'm', 'n', 'o', 'r', 'x', 'z', '|']
+
+    chars_to_check_regex_for = '"LM[!:|'
+    tokens = []
+    char_index = 0
+    while char_index < len(song_line):
+        char = song_line[char_index]
+        #if char is a comment we ignore the rest of the line
+        if char == '%':
+            break
+        if char in ignore_list:
+            char_index += 1
+            continue
+        if char in chars_to_check_regex_for:
+            #Check all regexex here
+            regex_matched = False
+            for regex in regex_list_ignore:
+                m = regex.match(song_line[char_index:])
+                if m:
+                    char_index = char_index + m.end()
+                    regex_matched = True
+                    break
+            if regex_matched:
+                continue
+            for regex in regex_list_tokens:
+                m = regex.match(song_line[char_index:])
+                if m:
+                    tok = song_line[char_index:char_index+m.end()]
+                    tok = tok.replace(" ", "")
+                    tokens.append(tok)
+                    char_index = char_index + m.end()
+                    regex_matched = True
+                    break
+            if regex_matched:
+                continue
+        if char in valid_single_tokens:
+            tokens.append(char)
+        char_index += 1
+    return tokens
 
 
 def create_csv_file(download_dir, output_file):
